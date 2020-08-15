c6084f99d9d78424da1e7b7d255b715a71fe1cbd
cai <1020233514@qq.com>
2020-08-15 12:39:39 +0800

    添加日志封装

--- baselibrary/src/main/java/com/example/baselibrary/utils/LogUtils.kt
+++ baselibrary/src/main/java/com/example/baselibrary/utils/LogUtils.kt
@@ -1,5 +1,72 @@
 package com.example.baselibrary.utils
 
-object LogUtils {
+import android.text.TextUtils
+import android.util.Log
 
+class LogUtils {
+
+    var className //类名
+            : String? = null
+    var methodName //方法名
+            : String? = null
+    var lineNumber //行数
+            = 0
+
+    private var TAG :String = "LogUtils";
+
+    /**
+     * 获取文件名、方法名、所在行数
+     * @param sElements
+     */
+    private fun getMethodNames(sElements: Array<StackTraceElement>) {
+        className = sElements[1].fileName
+        methodName = sElements[1].methodName
+        lineNumber = sElements[1].lineNumber
+    }
+
+    private fun createLog (log: String):String{
+        getMethodNames(Throwable().stackTrace)
+        val buffer = StringBuffer()
+        buffer.append("========$className : $methodName =======\r\n")
+        buffer.append(log)
+        buffer.append("========$lineNumber========\r\n")
+        return buffer.toString()
+    }
+
+    /**静态语法块*/
+    companion object{
+
+        private val instance : LogUtils by lazy(LazyThreadSafetyMode.NONE) {
+            LogUtils()
+        }
+
+
+        open fun v(log: String){
+            Log.v(instance.TAG,instance.createLog(log))
+        }
+
+        open fun i(log: String){
+            Log.i(instance.TAG,instance.createLog(log))
+        }
+
+        open fun d(log: String){
+            Log.d(instance.TAG,instance.createLog(log))
+        }
+
+        open fun e(log: String){
+            Log.e(instance.TAG,instance.createLog(log))
+        }
+
+        open fun w(log: String){
+            Log.w(instance.TAG,instance.createLog(log))
+        }
+
+        open fun wtf(log: String){
+            Log.wtf(instance.TAG,instance.createLog(log))
+        }
+
+        open fun setTAG(tag:String){
+            instance.TAG = tag;
+        }
+    }
 }
